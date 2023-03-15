1e0fbe1c421f4a37c973adf63317d945d8db62c1
austinjnr <austinobonyo@gmail.com>
2023-03-15 15:22:01 +0300

    migration generator

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Loading....."
+Pet.create([{name: "Timmy", breed: "Siamese"}, {name: "Kaws", breed: "Husky"}, {name: "Sicko", breed: "K-9"}])
+puts "completed!"

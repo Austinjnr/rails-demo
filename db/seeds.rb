3baaec6171476b893071dc710474982ed38bdbfa
austinjnr <austinobonyo@gmail.com>
2023-03-17 23:09:01 +0300

    rails generator

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-puts "Loading....."
-Pet.create([{name: "Timmy", breed: "Siamese"}, {name: "Kaws", breed: "Husky"}, {name: "Sicko", breed: "K-9"}])
-puts "completed!"
+# puts "Loading....."
+# Pet.create([{name: "Timmy", breed: "Siamese"}, {name: "Kaws", breed: "Husky"}, {name: "Sicko", breed: "K-9"}])
+# puts "completed!"
+
+Pet.create(name:Faker::Creature::Cat.name, breed:Faker::Creature::Cat.breed)
